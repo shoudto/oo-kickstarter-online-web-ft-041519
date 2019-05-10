97d629bc7b8f3133bdc52232d0f2d13f0fecccbc
Oscar Shoudt <shoudto@gmail.com>
2019-05-10 19:58:17 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,18 @@
+require 'pry'
+
+class Backer 
+  
+  attr_accessor :backed_projects
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+   # binding.pry 
+    @backed_projects << project 
+  end 
+  
+end 
